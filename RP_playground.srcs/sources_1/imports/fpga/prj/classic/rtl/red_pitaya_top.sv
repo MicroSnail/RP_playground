f3f05ec2a185ad46af4e5779590e4b4e32a1bdf8
Jialun <gzmicrosnail@gmail.com>
2017-01-03 15:15:44 -0500

    Try to build red pitaya from the ground up

---
+++ RP_playground.srcs/sources_1/imports/fpga/prj/classic/rtl/red_pitaya_top.sv
@@ -0,0 +1,145 @@
+////////////////////////////////////////////////////////////////////////////////
+// Red Pitaya TOP module. It connects external pins and PS part with
+// other application modules.
+// Authors: Matej Oblak, Iztok Jeras
+// (c) Red Pitaya  http://www.redpitaya.com
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * GENERAL DESCRIPTION:
+ *
+ * Top module connects PS part with rest of Red Pitaya applications.  
+ *
+ *                   /-------\      
+ *   PS DDR <------> |  PS   |      AXI <-> custom bus
+ *   PS MIO <------> |   /   | <------------+
+ *   PS CLK -------> |  ARM  |              |
+ *                   \-------/              |
+ *                                          |
+ *                            /-------\     |
+ *                         -> | SCOPE | <---+
+ *                         |  \-------/     |
+ *                         |                |
+ *            /--------\   |   /-----\      |
+ *   ADC ---> |        | --+-> |     |      |
+ *            | ANALOG |       | PID | <----+
+ *   DAC <--- |        | <---- |     |      |
+ *            \--------/   ^   \-----/      |
+ *                         |                |
+ *                         |  /-------\     |
+ *                         -- |  ASG  | <---+ 
+ *                            \-------/     |
+ *                                          |
+ *             /--------\                   |
+ *    RX ----> |        |                   |
+ *   SATA      | DAISY  | <-----------------+
+ *    TX <---- |        | 
+ *             \--------/ 
+ *               |    |
+ *               |    |
+ *               (FREE)
+ *
+ * Inside analog module, ADC data is translated from unsigned neg-slope into
+ * two's complement. Similar is done on DAC data.
+ *
+ * Scope module stores data from ADC into RAM, arbitrary signal generator (ASG)
+ * sends data from RAM to DAC. MIMO PID uses ADC ADC as input and DAC as its output.
+ *
+ * Daisy chain connects with other boards with fast serial link. Data which is
+ * send and received is at the moment undefined. This is left for the user.
+ */
+
+module red_pitaya_top #(
+  // identification
+  bit [0:5*32-1] GITH = '0,
+  // module numbers
+  int unsigned MNA = 2,  // number of acquisition modules
+  int unsigned MNG = 2   // number of generator   modules
+)(
+  // PS connections
+  inout  logic [54-1:0] FIXED_IO_mio     ,
+  inout  logic          FIXED_IO_ps_clk  ,
+  inout  logic          FIXED_IO_ps_porb ,
+  inout  logic          FIXED_IO_ps_srstb,
+  inout  logic          FIXED_IO_ddr_vrn ,
+  inout  logic          FIXED_IO_ddr_vrp ,
+  // DDR
+  inout  logic [15-1:0] DDR_addr   ,
+  inout  logic [ 3-1:0] DDR_ba     ,
+  inout  logic          DDR_cas_n  ,
+  inout  logic          DDR_ck_n   ,
+  inout  logic          DDR_ck_p   ,
+  inout  logic          DDR_cke    ,
+  inout  logic          DDR_cs_n   ,
+  inout  logic [ 4-1:0] DDR_dm     ,
+  inout  logic [32-1:0] DDR_dq     ,
+  inout  logic [ 4-1:0] DDR_dqs_n  ,
+  inout  logic [ 4-1:0] DDR_dqs_p  ,
+  inout  logic          DDR_odt    ,
+  inout  logic          DDR_ras_n  ,
+  inout  logic          DDR_reset_n,
+  inout  logic          DDR_we_n   ,
+
+  // Red Pitaya periphery
+
+  // ADC
+  input  logic [MNA-1:0] [16-1:2] adc_dat_i,  // ADC data
+  input  logic           [ 2-1:0] adc_clk_i,  // ADC clock {p,n}
+  output logic           [ 2-1:0] adc_clk_o,  // optional ADC clock source (unused)
+  output logic                    adc_cdcs_o, // ADC clock duty cycle stabilizer
+  // DAC
+  output logic [14-1:0] dac_dat_o  ,  // DAC combined data
+  output logic          dac_wrt_o  ,  // DAC write
+  output logic          dac_sel_o  ,  // DAC channel select
+  output logic          dac_clk_o  ,  // DAC clock
+  output logic          dac_rst_o  ,  // DAC reset
+  // PWM DAC
+  output logic [ 4-1:0] dac_pwm_o  ,  // 1-bit PWM DAC
+  // XADC
+  input  logic [ 5-1:0] vinp_i     ,  // voltages p
+  input  logic [ 5-1:0] vinn_i     ,  // voltages n
+  // Expansion connector
+  inout  logic [ 8-1:0] exp_p_io   ,
+  inout  logic [ 8-1:0] exp_n_io   ,
+  // SATA connector
+  output logic [ 2-1:0] daisy_p_o  ,  // line 1 is clock capable
+  output logic [ 2-1:0] daisy_n_o  ,
+  input  logic [ 2-1:0] daisy_p_i  ,  // line 1 is clock capable
+  input  logic [ 2-1:0] daisy_n_i  ,
+  // LED
+  inout  logic [ 8-1:0] led_o
+);
+
+// diferential clock input
+IBUFDS i_clk (.I (adc_clk_i[1]), .IB (adc_clk_i[0]), .O (adc_clk_in));  // differential clock input
+
+
+zynq_block_wrapper zynqchip(
+    .DDR_addr(DDR_addr),
+    .DDR_ba(DDR_ba),
+    .DDR_cas_n(DDR_cas_n),
+    .DDR_ck_n(DDR_ck_n),
+    .DDR_ck_p(DDR_ck_p),
+    .DDR_cke(DDR_cke),
+    .DDR_cs_n(DDR_cs_n),
+    .DDR_dm(DDR_dm),
+    .DDR_dq(DDR_dq),
+    .DDR_dqs_n(DDR_dqs_n),
+    .DDR_dqs_p(DDR_dqs_p),
+    .DDR_odt(DDR_odt),
+    .DDR_ras_n(DDR_ras_n),
+    .DDR_reset_n(DDR_reset_n),
+    .DDR_we_n(DDR_we_n),
+    .FIXED_IO_ddr_vrn(FIXED_IO_ddr_vrn),
+    .FIXED_IO_ddr_vrp(FIXED_IO_ddr_vrp),
+    .FIXED_IO_mio(FIXED_IO_mio),
+    .FIXED_IO_ps_clk(FIXED_IO_ps_clk),
+    .FIXED_IO_ps_porb(FIXED_IO_ps_porb),
+    .FIXED_IO_ps_srstb(FIXED_IO_ps_srstb)
+  );
+
+
+
+assign led_o = {8'b00110011};
+
+endmodule
---
+++ RP_playground.srcs/sources_1/new/FIR_filter.sv
@@ -0,0 +1,171 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/30/2016 09:57:40 AM
+// Design Name: 
+// Module Name: FIR_filter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module FIR_filter
+  #(
+      parameter DW = 16,            //data width
+      parameter out_DW = 2*DW,      //output width
+      parameter nDatMAC= 32,        //number of data each MAC handle
+      parameter nMAC = 50           //number of MACs
+  )
+  (
+    input       [DW - 1 : 0]                datIn,
+    output reg  [out_DW - 1 : 0]            datOut,
+    input                                   clk,
+    input                                   rst,
+    // System bus
+    input      [ 32-1:0] sys_addr   ,  // bus address
+    input      [ 32-1:0] sys_wdata  ,  // bus write data
+    input      [  4-1:0] sys_sel    ,  // bus write byte select
+    input                sys_wen    ,  // bus write enable
+    input                sys_ren    ,  // bus read enable
+    output reg [ 32-1:0] sys_rdata  ,  // bus read data
+    output reg           sys_err    ,  // bus error indicator
+    output reg           sys_ack       // bus acknowledge signal    
+  );
+
+    // total number of sample/ size of the sample array is
+    // (nDatMAC * nMAC)
+    // The total number of bits in the flatten array then 
+    // is given by (nDatMAC * nMAC * dataBitwidth)
+    localparam totalNBits = nDatMAC * DW * nMAC;
+    localparam totalNSamples = nDatMAC * nMAC;
+    localparam nBits_MAC  = nDatMAC * DW;
+//-------------------End of parameters-----------------//
+
+
+
+    reg   [totalNBits - 1 : 0]      flatCoeff = 0;
+//    reg   [dataBitwidth * nData - 1 : 0]  flatSample;
+    reg   [out_DW * nMAC - 1 : 0]   mac_out_storage = 0;
+    wire  [out_DW * nMAC - 1 : 0]   mac_direct_out;
+    wire   [out_DW - 1 : 0]         mac_sum;
+    wire  [nMAC - 1: 0]             mac_armed;
+    wire  [nMAC - 1: 0]             mac_done;
+    wire                            sum_finished;
+    wire                            sum_armed;
+
+//------Sampler----------------------------------------//
+    reg   [totalNBits - 1 : 0]  flatSample  = 0;
+    reg                         sampled     = 0;
+    wire  [DW - 1 : 0]          newSample;
+    wire                        rst_sampler = 0;
+    
+    always @(posedge clk) begin
+      if (&mac_armed & ~sampled) begin 
+        flatSample <= {newSample, flatSample[totalNBits - 1 : DW]};
+        sampled <= 1;
+      end else if (&mac_done) begin
+        sampled <= 0;
+      end
+    end
+
+//------Sampler end------------------------------------//
+
+//-----------MAC output storage -----------------------//
+//    reg mac_out_ready = 0;
+    reg mac_out_storage_updated = 0;
+    
+    always @(posedge clk) begin    
+      // Only update the MAC results if mac_done is 
+      // flagged and the storage is not updated
+      // storage_updated flag need to be reset somewhere
+      if(mac_done && ~mac_out_storage_updated ) begin 
+        mac_out_storage <= mac_direct_out;
+//        mac_out_ready   <= 1;
+        mac_out_storage_updated <= 1;
+      end else if (sum_finished) begin
+        mac_out_storage_updated <= 0;
+      end
+    end
+//-----------MAC output storage end--------------------//
+
+
+//----Instantiate multiple MACs -----------------------//
+  multiplyAccumulator 
+  #(
+    .nData(nDatMAC),
+    .datBW(DW), 
+    .outBW(out_DW)
+  ) MACs [nMAC-1 : 0]
+    (
+      .inA(flatCoeff),
+      .inB(flatSample),
+      .inputUpdated(sampled),
+      .clk(clk),
+      .rst(1'b0),
+      .mac_done(mac_done),
+      .mac_armed(mac_armed),
+      .mac_out(mac_direct_out)
+    );        
+//----Instantiate multiple MACs end--------------------//
+
+//------MAC sum----------------------------------------//
+  serial_sum #(.nMAC(nMAC), .datBW(DW), .outBW(out_DW))
+    inst_serial_sum 
+      (
+         .mac_output(mac_out_storage),
+         .inputUpdated(mac_out_storage_updated),
+         .clk(clk),
+         .rst(0),
+         .mac_sum(mac_sum),
+         .sum_finished(sum_finished)
+      );
+//------MAC sum end------------------------------------//
+
+
+
+
+
+// System bus connection
+int i;
+
+always @(posedge clk)
+if (rst == 1'b0) begin
+  flatCoeff <= {(totalNBits * - 1) {1'b0}};
+end else if (sys_wen) begin
+  for (i=0; i < totalNSamples; i = i+1) begin
+    if (sys_addr[19:0]== (20'h0 + i * 4))   flatCoeff[i * DW +: DW] <= sys_wdata[ DW-1 : 0];
+  end
+end
+
+wire sys_en;
+assign sys_en = sys_wen | sys_ren;
+
+int j;
+always @(posedge clk)
+if (rst == 1'b0) begin
+  sys_err <= 1'b0;
+  sys_ack <= 1'b0;
+end else begin
+  sys_err <= 1'b0;
+
+  for (j=0; j < totalNSamples; j = j+1) begin
+    if (sys_addr[19:0]== (20'h0 + j * 4)) begin
+      sys_ack   <= sys_en;
+      sys_rdata <= { {(32-DW){1'b0}}, flatCoeff[j * DW +: DW]};
+    end
+  end
+end
+
+
+endmodule
